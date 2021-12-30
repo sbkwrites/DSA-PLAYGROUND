24fefc11ba31647bc37763410178f74306b738ff
Sai Balaji <sbkwrites@gmail.com>
2021-12-30 10:27:03 +0530

    "Recursion Example"

--- src/main/java/com/problems/recursion/Recursion01.java
+++ src/main/java/com/problems/recursion/Recursion01.java
@@ -3,12 +3,28 @@ package com.problems.recursion;
 public class Recursion01 {
     public static void main(String[] args) {
 
+      //  recFun(1);
+        fibanocci(1);
     }
 
-    static void recFun()
+
+
+    static void recFun(int n)
     {
-
+        if(n==6){
+            return;
+        }
+        System.out.println(n);
+        recFun(1+n);
     }
 
 
+
+    private static int fibanocci(int x)
+    {
+        if(x<2){
+            return x;
+        }
+        return fibanocci(x-1)+fibanocci(x-2);
+    }
 }
