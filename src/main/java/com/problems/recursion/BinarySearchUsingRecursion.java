de773144be05d7d1e2bccae5df33ca20cbc05005
Sai Balaji <sbkwrites@gmail.com>
2021-12-30 19:03:01 +0530

    "Recursion Example"

---
+++ src/main/java/com/problems/recursion/BinarySearchUsingRecursion.java
@@ -0,0 +1,34 @@
+package com.problems.recursion;
+
+public class BinarySearchUsingRecursion {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,25,55,66,77,88,99,199};
+
+        int target = 77;
+        System.out.println(search(arr,target,0, arr.length-1));
+    }
+
+    static int search(int[] arr, int target,int s , int e){
+
+        if(s>e){
+            return -1;
+        }
+        int mid = s + (e-s)/2;
+
+        if(arr[mid]==target){
+            return mid;
+        }
+
+
+        if(target > arr[mid]){
+            return search(arr, target, mid+1, e);
+        }
+        return search(arr, target, s, mid-1);
+
+
+
+
+    }
+}
