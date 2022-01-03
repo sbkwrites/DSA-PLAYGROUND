e4bae28a4e59c2fa31fd450e3b397c1c3141460e
Sai Balaji <sbkwrites@gmail.com>
2022-01-03 20:33:06 +0530

    "Find Range Example"

--- src/main/java/com/problems/binarysearch/PracBinarySearch.java
+++ src/main/java/com/problems/binarysearch/PracBinarySearch.java
@@ -1,16 +1,19 @@
-package com.searchingAlgo;
+package com.problems.binarysearch;
+
+import java.util.Arrays;
 
 public class PracBinarySearch {
     public static void main(String[] args) {
-        int arr[] = {-22, -12 , -8 , 0 , 5 ,9 , 14 , 21 , 48, 67 ,89 ,101};
-        int target = 48;
+        int arr[] = {1, 2, 3, 3, 3, 4, 4, 5};
+        int target = 3;
 
 
-        System.out.println(search(target,arr));
+        System.out.println(Arrays.toString(search(target,arr)));
     }
 
-    public static int search(int x , int... arr){
+    int search(int x , int[] arr,boolean findStart){
 
+        int ans = 0;
         int start = 0;
         int end = arr.length-1;
 
@@ -19,8 +22,13 @@ public class PracBinarySearch {
         int midpoint = start + (end - start) /2;
 
         if(x == arr[midpoint]){
+            ans = midpoint;
 
-            return arr[midpoint];
+            if(findStart)
+                end = midpoint-1;
+            else
+                start = midpoint+1;
+
         }
         else if (x> arr[midpoint]){
             start = midpoint+1;
@@ -33,9 +41,28 @@ public class PracBinarySearch {
         }
 
 
-        return 0;
+        return ans;
 
 
     }
+
+
+    public static int[] search(int x , int... arr){
+
+        int[] ans = {-1,-1};
+
+        ans[0] = search(x,  arr,true);
+        ans[1]= search(x,  arr,false);
+
+
+
+        return ans;
+
+    }
+
+
+
+
+
 
 }
