541c6ed6f1fb73a708e10cf4f4ba5c900f751ee4
Sai Balaji <sbkwrites@gmail.com>
2022-01-13 21:29:42 +0530

    "Araay "

---
+++ src/main/java/co/MainClass.java
@@ -0,0 +1,13 @@
+package co;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+    }
+    int[] getSquareSortedArray (int[] arr) {
+        // add your logic here
+
+
+        return new int[2];
+    }
+}
