685a393a6f80800668168307c8e0d05274e41e6c
Sai Balaji <sbkwrites@gmail.com>
2022-01-17 09:59:50 +0530

    "max consq numbs"

--- src/main/java/com/problems/arrays/FindMaxConsqNumber.java
+++ src/main/java/com/problems/arrays/FindMaxConsqNumber.java
@@ -1,30 +1,38 @@
 package com.problems.arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindMaxConsqNumber {
 
     public static void main(String[] args) {
         int[] arr = {1, 1, 3, 2, 3, 1, 1, 1};
 
-        System.out.println(getMaxConsecutiveOnes(arr));
+        System.out.println(getMaxConsecutiveOnes(arr,0,0));
     }
 
-   static int getMaxConsecutiveOnes(int[] A) {
-        // add your logic here
-       int ans = 0;
+   static int getMaxConsecutiveOnes(int[] A,int counter,int ans) {
+       // add your logic here
 
-       for (int i = 0; i<A.length;i++)
+       List<Integer> numbers = new ArrayList<Integer>();
+
+
+       if (counter == A.length - 1)
        {
-           if(A[i]==1){
-
-           }
-
+           return ans;
+       }
+       if (A[counter] == 1) {
+           counter++;
+           ans++;
+           getMaxConsecutiveOnes(A, counter, ans);
+       } else {
+           counter++;
+           getMaxConsecutiveOnes(A, counter, ans);
        }
 
 
+        return -1;
 
-
-
-       return  ans;
-    }
+   }
 
 }
