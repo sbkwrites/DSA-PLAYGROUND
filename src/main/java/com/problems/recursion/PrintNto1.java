d87412082936390feef51a2bb08a28c043bd6494
Sai Balaji <sbkwrites@gmail.com>
2022-01-24 10:23:49 +0530

    "Recursion examples"

---
+++ src/main/java/com/problems/recursion/PrintNto1.java
@@ -0,0 +1,16 @@
+package com.problems.recursion;
+
+public class PrintNto1 {
+
+    public static void main(String[] args) {
+        fun(5);
+    }
+
+    static void fun(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        fun(n-1);
+    }
+}
